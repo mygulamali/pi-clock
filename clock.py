c058b48a4d8de6f5a01dee37b1b43506d062a379
Murtaza Gulamali <murtaza@gulamali.net>
2023-03-18 23:55:01 +0000

    Add clock app

---
+++ clock.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import datetime
+import time
+
+from microdotphat import clear, set_brightness, set_decimal, show, write_string
+
+
+set_brightness(0.1)
+
+while True:
+    clear()
+    t = datetime.datetime.now()
+    if t.second % 2 == 0:
+        set_decimal(2, 1)
+        set_decimal(4, 1)
+    else:
+        set_decimal(2, 0)
+        set_decimal(4, 0)
+    write_string(t.strftime('%H%M%S'), kerning=False)
+    show()
+    time.sleep(0.05)
